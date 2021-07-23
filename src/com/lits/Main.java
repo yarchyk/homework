170861de34136782b89ef987676a7aacbe81ac1d
yarchyk <yarkosvyshch2@gmail.com>
2021-07-23 14:09:01 +0200

    Git init

---
+++ src/com/lits/Main.java
@@ -0,0 +1,44 @@
+package com.lits;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte a = 1;
+        byte b = 4;
+        byte c = 3;
+        byte e = 2;
+        byte i = 5;
+        byte z = 6;
+        byte x = 7;
+        byte p = 8;
+        byte o = 9;
+        byte I = 10;
+        System.out.println("a+b/e+c*(i+a)=" + (byte)(a + b / e + c * (i + a)));
+        System.out.println("z+x-e+p/a=" + (byte) ( z + x - e + (p/a)));
+        System.out.println("o+i+I*(a+b)=" + (byte) (o+i+I*(a+b)));
+
+
+        char A1 = 14;
+        char A2 = 77;
+        char A3 = 105;
+        System.out.println((char)14);
+        System.out.println((char)77);
+        System.out.println((char)105);
+
+
+
+     boolean w = 10%2 == 0;
+        System.out.println("Number 10 is even -" + (w));
+        boolean f = 13%2 == 0;
+        System.out.println("Number 13 is even -" + (f));
+        boolean q = 17%2 == 0;
+        System.out.println("Number 17 is even -" + (q));
+        boolean m = 20%2 == 0;
+        System.out.println("Number 20 is even -" + (m));
+
+
+
+
+    }
+}
